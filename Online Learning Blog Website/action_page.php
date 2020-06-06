df9827bc8204038cd4830c21db564c54c9b2ccdc
Imran <mohdmeran99@gmail.com>
2020-06-06 12:33:27 +0800

    add all file of my website interface

---
+++ Online Learning Blog Website/action_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>E-Learning: Action</title>
+	<link rel="icon" href = "webIcon.png" type = "image/icon" size = "16x16">
+
+	<style>
+	</style>
+</head>
+<body>
+
+	<div>
+		<?php
+		echo
+		"<h1>Hello $_POST["name"]<h1>
+		<p>$_POST["subject"]</p>";
+		?>
+	</div>
+</body>
+</html>
